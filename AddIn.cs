035ddda1fcac4c249ed26bfe7193c7adbace6250
Govert van Drimmelen <govert@dnakode.com>
2022-01-15 18:00:59 +0200

    Add project files.

---
+++ AddIn.cs
@@ -0,0 +1,23 @@
+﻿using ExcelDna.Integration;
+using ExcelDna.IntelliSense;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIntellisense
+{
+    internal class AddIn : IExcelAddIn
+    {
+        public void AutoClose()
+        {
+            IntelliSenseServer.Uninstall();
+        }
+
+        public void AutoOpen()
+        {
+            IntelliSenseServer.Install();
+        }
+    }
+}
---
+++ Functions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIntellisense
+{
+    public class Functions
+    {
+        public static object GetPartDescription(string pn) => pn;
+    }
+}
